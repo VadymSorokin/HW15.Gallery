57a991cd0cc9f1f9dc5a54f889ef696015c248f0
VadymSorokin <s.a.rock.in619@gmail.com>
2021-03-14 20:09:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,19 @@ const albumPhotos = document.querySelector('.album__photos');
 
 function init() {
 	renderAlbums();
+	getFirstAlbumId()
 	renderPhotos();
 	renderAlbumPhotoEventListener();
 }
 init()
 
-function renderAlbums() {
-	fetch('https://jsonplaceholder.typicode.com/albums')
+function sendGetRequestAlbums() {
+	return fetch('https://jsonplaceholder.typicode.com/albums')
 		.then((response) => response.json())
+}
+
+function renderAlbums() {
+	sendGetRequestAlbums()
 		.then((album) => {
 			for (let i = 0; i < album.length; i++) {
 				const albumItem = `<li data-id-number="${[i + 1]}">album ${album[i].id}: ${album[i].title}</li>`;
@@ -19,8 +24,8 @@ function renderAlbums() {
 		})
 }
 
-function renderPhotos(albumId = 1) {
-	fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`)
+function renderPhotos(albumId) {
+	fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${(albumId)}`)
 		.then((response) => response.json())
 		.then((photoList) => {
 			for (let i = 0; i < photoList.length; i++) {
@@ -43,3 +48,13 @@ function clearAlbum() {
 		albumPhotos.removeChild(albumPhotos.firstChild);
 	}
 }
+
+function getFirstAlbumId() {
+	return sendGetRequestAlbums()
+		.then((response) => renderPhotos(response[0].id))
+}
+// Пытался использовать эту функцию , но она не срабатывает. Пишет , что null .
+// Хотя функция срабатывает , когда вставлял ее в  renderAlbumPhotoEventListener.
+//function getFirstAlbumId() {
+//	return albumList.firstElementChild.dataset.idNumber;
+//}
